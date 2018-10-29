753fee433577af4240d10069b50f050451d704d0
WGillian <gillian@z-dev.com>
2018-10-29 13:48:34 +0000

    Adds react-spring and animation

--- src/components/itemList/index.js
+++ src/components/itemList/index.js
@@ -5,6 +5,8 @@ import Text from 'components/core/text'
 import CheckBox from 'components/core/checkBox'
 import './style.scss'
 
+import { Trail } from 'react-spring'
+
 const Item = props => (
   <Div addStyle="item">
     <CheckBox onChange={() => props.onChange(props.id)} checked={_.includes(props.selectedItems, props.id)} />
@@ -12,8 +14,18 @@ const Item = props => (
   </Div>
 )
 
-export default props => (
-  <Div>
-    {_.map(props.itemIds, id => <Item id={id} key={id} onChange={props.onToggleItem} {...props} />)}
-  </Div>
-)
+export default props => {
+  const items = _.map(props.itemIds, id => <Item id={id} key={id} onChange={props.onToggleItem} {...props} />)
+  return (
+    <Div addStyle="itemListContainer">
+      <Trail
+        items={items} keys={item => item.key}
+        from={{ transform: 'translate3d(0,-200px,0)' }}
+        to={{ transform: 'translate3d(0,0px,0)' }}>
+          {item => props =>
+            <span style={props}>{item}</span>
+          }
+      </Trail>
+    </Div>
+  )
+}
--- src/components/linkList/index.js
+++ src/components/linkList/index.js
@@ -4,20 +4,44 @@ import Div from 'components/core/div'
 import Text from 'components/core/text'
 import './style.scss'
 
-const LinkItem = props => (
-  <li className="listLinkItem">
-    <Div addStyle="linkItem">
-      <a href={props.isEnabled ? props.link : null}>Link {props.id} </a>
-      <Text> - Enabled when </Text>
-      <Text addStyle="condition"> {props.condition}</Text>
-    </Div>
-  </li>
-)
+import { Spring, Trail } from 'react-spring'
 
-export default props => (
-  <Div>
-    <ol>
-      {_.map(props.links, link => <LinkItem id={link.id} key={link.id} condition={link.condition} link={props.link} isEnabled={link.isEnabled} />)}
-    </ol>
-  </Div>
-)
+const LinkProgress = props => {
+  const [ goFrom, to ] = props.item.props.isEnabled ? [{ background: '#fff' }, { background: '#50c878' }] : [{ background: '#50c878' }, { background: '#fff' }]
+  return (
+    <Spring from={goFrom} to={to}>
+      {({background}) => <Div style={{background, ...props.trailStyle}} addStyle="linkSpring">{props.item}</Div>}
+    </Spring>
+  )
+}
+
+const LinkItem = props => {
+  return (
+    <li className="listLinkItem">
+      <Div addStyle="linkItem">
+        <a href={props.isEnabled ? props.link : null}>Link {props.id} </a>
+        <Text> - Enabled when </Text>
+        <Text addStyle="condition">{props.condition}</Text>
+      </Div>
+    </li>
+  )
+}
+
+export default props => {
+  const items = _.map(props.links, link => <LinkItem id={link.id} key={link.id} condition={link.condition} link={props.link} isEnabled={link.isEnabled} />)
+  return (
+    <Div addStyle="linkListContainer">
+      <ol>
+        <Trail
+          items={items} keys={item => item.key}
+          from={{ transform: 'translate3d(-100px, 0, 0)' }}
+          to={{ transform: 'translate3d(0,0px,0)' }}>
+            {item => props =>
+              <LinkProgress trailStyle={props} item={item}/>
+            }
+          </Trail>
+      </ol>
+    </Div>
+  )
+}
+
